1c8881731ffeb7100daf5ccc488e2e25057c8b77
dckat <kdc9619@naver.com>
2024-03-18 17:05:03 +0900

    feat: 엔드포인트 권한 부여 재정의 구현

--- src/main/java/com/meerkat/security/config/ProjectConfig.java
+++ src/main/java/com/meerkat/security/config/ProjectConfig.java
@@ -3,12 +3,17 @@ package com.meerkat.security.config;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+
+/* 기본 구성 클래스 정의
 @Configuration
 public class ProjectConfig {
 
@@ -32,5 +37,23 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+}
+*/
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        /* 모든 요청 인증
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+        */
+
+        http.authorizeRequests()
+                .anyRequest().permitAll(); // 인증없이 요청 가능
     }
 }

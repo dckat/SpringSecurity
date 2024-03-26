9aa22166add08c71fc4966a49a1aa12ddac3bc4c
dckat <kdc9619@naver.com>
2024-03-26 16:00:29 +0900

    chore: 불필요한 코드 및 클래스 제거

--- src/main/java/com/meerkat/ss/config/ProjectConfig.java
+++ src/main/java/com/meerkat/ss/config/ProjectConfig.java
@@ -1,66 +1,14 @@
 package com.meerkat.ss.config;
 
-import com.meerkat.ss.domain.User;
 import com.meerkat.ss.security.CustomAuthenticationProvider;
-import com.meerkat.ss.service.InMemoryUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.proxy.NoOp;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
-import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
-import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import javax.sql.DataSource;
-import java.util.List;
 
 
-@Configuration
-public class ProjectConfig {
 
-    /* JDBC
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }
-    */
-
-    // LDAP
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var cs = new DefaultSpringSecurityContextSource(
-                "ldap://localhost:33389/dc=springframework,dc=org"
-        );
-        cs.afterPropertiesSet();
-
-        var manager = new LdapUserDetailsManager(cs);
-
-        manager.setUsernameMapper(
-                new DefaultLdapUsernameToDnMapper("ou=groups", "uid")
-        );
-
-        manager.setGroupSearchBase("ou=groups");
-
-        return manager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-}
-
-
-/* AuthenticationProvider 활용한 구성 클래스
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
@@ -78,69 +26,3 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().anyRequest().authenticated();
     }
 }
-*/
-
-/* UserDetailsService.PasswordEncoder 정의 예제 클래스
-@Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
-
-    // PasswordEncoder를 빈으로 설계
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        // 사용자를 인메모리에 저장하기 위한 UserDetailsService 선언
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        // 사용자 정의
-        var user = User.withUsername("kim")
-                .password("test")
-                .authorities("read")
-                .build();
-
-        userDetailsService.createUser(user);
-
-        auth.userDetailsService(userDetailsService);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-
-        http.authorizeRequests()
-                .anyRequest().authenticated();  // 모든 요청에 인증
-    }
-}
-*/
-
-/* 기본 구성 정의 예제 클래스
-@Configuration
-public class ProjectConfig {
-
-    // UserDetailsService 빈에 대한 구성
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        // UserDetailService에 필요한 사용자 생성
-        var user = User.withUsername("kim")
-                .password("test")
-                .authorities("read")
-                .build();
-
-        userDetailsService.createUser(user);
-
-        return userDetailsService;
-    }
-
-    // PasswordEncoder 컨텍스트에 추가
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-}
-*/

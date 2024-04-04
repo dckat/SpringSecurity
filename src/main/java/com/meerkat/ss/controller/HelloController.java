adf36be3c9f0eee34915f8945a5b0b48c133d5c0
dckat <kdc9619@naver.com>
2024-04-04 14:59:16 +0900

    feat: DelegatingSecurityContextRunnable 활용 보안 컨텍스트 전달 구현

--- src/main/java/com/meerkat/ss/controller/HelloController.java
+++ src/main/java/com/meerkat/ss/controller/HelloController.java
@@ -1,11 +1,17 @@
 package com.meerkat.ss.controller;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class HelloController {
@@ -23,7 +29,22 @@ public class HelloController {
     public void goodbye() {
         SecurityContext context = SecurityContextHolder.getContext();
         String username = context.getAuthentication().getName();
+    }
 
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
 
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        try {
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Ciao, " + executorService.submit(contextTask).get() + "!";
+        } finally {
+            executorService.shutdown();
+        }
     }
 }

2142b427ab7b6f78ae6fd92b4ae259a0375cb272
dckat <kdc9619@naver.com>
2024-04-04 14:52:28 +0900

    feat: InitializingBean 활용 보안 컨텍스트 모드 설정 구현

--- src/main/java/com/meerkat/ss/config/ProjectConfig.java
+++ src/main/java/com/meerkat/ss/config/ProjectConfig.java
@@ -1,28 +1,23 @@
 package com.meerkat.ss.config;
 
-import com.meerkat.ss.security.CustomAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+
+import javax.sql.DataSource;
 
 
 @Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
+@EnableAsync
+public class ProjectConfig {
 
-    @Autowired
-    private CustomAuthenticationProvider authenticationProvider;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationProvider);
+    @Bean
+    public InitializingBean initializingBean() {
+        return () -> SecurityContextHolder.setStrategyName(
+                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeRequests().anyRequest().authenticated();
-    }
 }

23e6d8f3785b4518ef04fb38108fb79e90274d1b
dckat <kdc9619@naver.com>
2024-04-05 15:37:55 +0900

    feat: 커스터마이징한 엔드포인트를 설정 클래스에 구현

--- src/main/java/com/meerkat/ss/config/ProjectConfig.java
+++ src/main/java/com/meerkat/ss/config/ProjectConfig.java
@@ -1,12 +1,10 @@
 package com.meerkat.ss.config;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.annotation.Bean;
+import com.meerkat.ss.security.CustomEntryPoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 
 
@@ -18,6 +16,7 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic(c -> {
             c.realmName("OTHER");
+            c.authenticationEntryPoint(new CustomEntryPoint());
         });
 
         http.authorizeRequests().anyRequest().authenticated();

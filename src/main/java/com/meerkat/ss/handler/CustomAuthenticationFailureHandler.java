3ed21ac4614609026426b0a4c22e1262f7300372
dckat <kdc9619@naver.com>
2024-04-05 15:57:25 +0900

    chore: 클래스에 컴포넌트 어노테이션 추가

--- src/main/java/com/meerkat/ss/handler/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/meerkat/ss/handler/CustomAuthenticationFailureHandler.java
@@ -2,6 +2,7 @@ package com.meerkat.ss.handler;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+@Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest httpServletRequest,

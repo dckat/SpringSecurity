59d9584f463818c1cf33681db2835c54cb187c1a
dckat <kdc9619@naver.com>
2024-04-08 14:45:15 +0900

    feat: 구성클래스 구현

--- src/main/java/com/meerkat/ss/config/ProjectConfig.java
+++ src/main/java/com/meerkat/ss/config/ProjectConfig.java
@@ -1,47 +1,45 @@
 package com.meerkat.ss.config;
 
-import com.meerkat.ss.handler.CustomAuthenticationFailureHandler;
-import com.meerkat.ss.handler.CustomAuthenticationSuccessHandler;
-import com.meerkat.ss.security.CustomEntryPoint;
+import com.meerkat.ss.service.AuthenticationProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 
 @Configuration
-@EnableAsync
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+    private AuthenticationProviderService authenticationProvider;
 
-    @Autowired
-    private CustomAuthenticationFailureHandler authenticationFailureHandler;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .formLogin()
-                        .successHandler(authenticationSuccessHandler)
-                                .failureHandler(authenticationFailureHandler)
-                                        .and()
-                                                .httpBasic();
-
-        http.authorizeRequests().anyRequest().authenticated();
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
-    /* 인증 실패 시 메시지 구현
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic(c -> {
-            c.realmName("OTHER");
-            c.authenticationEntryPoint(new CustomEntryPoint());
-        });
-
-        http.authorizeRequests().anyRequest().authenticated();
+    @Bean
+    public SCryptPasswordEncoder sCryptPasswordEncoder() {
+        return new SCryptPasswordEncoder();
     }
-    */
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+
+        http.formLogin()
+                .defaultSuccessUrl("/main", true);
+
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+    }
 }

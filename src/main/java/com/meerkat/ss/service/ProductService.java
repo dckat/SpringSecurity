7c9ba0a26d9a376051f844626fb5594193bc18dd
dckat <kdc9619@naver.com>
2024-04-08 14:50:50 +0900

    feat: Product 서비스 구현

---
+++ src/main/java/com/meerkat/ss/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.meerkat.ss.service;
+
+import com.meerkat.ss.entity.Product;
+import com.meerkat.ss.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}

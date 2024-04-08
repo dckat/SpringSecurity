085c64fd7a9de450b1ac3b5a1bace25fdc3f725e
dckat <kdc9619@naver.com>
2024-04-08 14:49:06 +0900

    feat: product 엔티티 클래스 및 열거형 구현

---
+++ src/main/java/com/meerkat/ss/entity/Product.java
@@ -0,0 +1,52 @@
+package com.meerkat.ss.entity;
+
+
+import com.meerkat.ss.entity.enums.Currency;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private double price;
+
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+}
---
+++ src/main/java/com/meerkat/ss/entity/enums/Currency.java
@@ -0,0 +1,5 @@
+package com.meerkat.ss.entity.enums;
+
+public enum Currency {
+    USD, GBP, EUR
+}

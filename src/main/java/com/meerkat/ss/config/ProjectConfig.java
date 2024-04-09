bd3e2f4b0d7e4c27575fb7cad6ef11d3c9af1dd6
dckat <kdc9619@naver.com>
2024-04-09 13:28:33 +0900

    feat: 권한에 따른 엔드포인트 접근 구현

--- src/main/java/com/meerkat/ss/config/ProjectConfig.java
+++ src/main/java/com/meerkat/ss/config/ProjectConfig.java
@@ -8,13 +8,53 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var manager = new InMemoryUserDetailsManager();
+
+        var user1 = User.withUsername("Kim")
+                .password("test")
+                .authorities("READ")
+                .build();
+
+        var user2 = User.withUsername("Park")
+                .password("test")
+                .authorities("WRITE")
+                .build();
+
+        manager.createUser(user1);
+        manager.createUser(user2);
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        http.authorizeRequests()
+                .anyRequest()
+                .hasAuthority("WRITE");     // WRITE 권한이 있는 사용자만 엔드포인트 접근
+    }
+
+    /*
     @Autowired
     private AuthenticationProviderService authenticationProvider;
 
@@ -32,4 +72,5 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .anyRequest().authenticated();
     }
+    */
 }

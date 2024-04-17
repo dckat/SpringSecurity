baf4ddd02fbc112c9ed90a81a2f43e3a61dbc87d
dckat <kdc9619@naver.com>
2024-04-17 16:47:52 +0900

    feat: 구현한 필터를 구성클래스에 추가

--- src/main/java/com/meerkat/ss/config/ProjectConfig.java
+++ src/main/java/com/meerkat/ss/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.meerkat.ss.config;
 
 import com.meerkat.ss.filter.AuthenticationLoggingFilter;
 import com.meerkat.ss.filter.RequestValidationFilter;
+import com.meerkat.ss.filter.StaticKeyAuthenticationFilter;
 import com.meerkat.ss.service.AuthenticationProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +25,17 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private StaticKeyAuthenticationFilter filter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterAt(filter, BasicAuthenticationFilter.class)
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
+
+    /* 기존 필터의 앞.뒤로 필터를 구성한 메소드 예시
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.addFilterBefore(
@@ -35,6 +47,7 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().permitAll();
     }
+    */
 
     /*
     @Autowired

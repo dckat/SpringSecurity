85a21a673bc6fdf88c9c512acbf068e300fe0d2f
dckat <kdc9619@naver.com>
2024-04-17 16:19:12 +0900

    feat: 인증 전 구현한 필터 구성

--- src/main/java/com/meerkat/ss/config/ProjectConfig.java
+++ src/main/java/com/meerkat/ss/config/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.meerkat.ss.config;
 
+import com.meerkat.ss.filter.RequestValidationFilter;
 import com.meerkat.ss.service.AuthenticationProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -16,57 +17,19 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var manager = new InMemoryUserDetailsManager();
-
-        var user1 = User.withUsername("kim")
-                .password("test")
-                .roles("ADMIN")
-                .build();
-
-        var user2 = User.withUsername("park")
-                .password("test")
-                .roles("MANAGER")
-                .build();
-
-        manager.createUser(user1);
-        manager.createUser(user2);
-
-        return manager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-
-        http.authorizeRequests()
-                .mvcMatchers("/product/{code:^[0-9]*$}")    // 길이와 관계없이 숫자를 포함한 문자열을 표현한 정규식
-                .permitAll()
-                .anyRequest()
-                .denyAll();
-
-        /*
-        http.authorizeRequests()
-                .mvcMatchers(HttpMethod.GET, "/a")
-                .authenticated()    // HTTP GET 방식으로 /a 요청시 사용자 인증
-                .mvcMatchers(HttpMethod.POST, "/a")
-                .permitAll()        // HTTP POST 방식으로 /a 요청시 모두 허용
-                .anyRequest()
-                .denyAll();         // 다른 경로에 대한 요청 거부
-
-        http.csrf().disable();  // HTTP POST 방식으로 /a 호출하도록 CSRF 비활성화
-        */
+        http.addFilterBefore(
+                new RequestValidationFilter(),
+                BasicAuthenticationFilter.class)
+                .authorizeRequests()
+                .anyRequest().permitAll();
     }
 
     /*

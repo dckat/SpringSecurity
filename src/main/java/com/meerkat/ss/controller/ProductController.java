927b991b08cf0dd079bb09bbcff7751415b46f5f
dckat <kdc9619@naver.com>
2024-04-18 16:42:04 +0900

    feat: RequestMapping 어노테이션 추가

--- src/main/java/com/meerkat/ss/controller/ProductController.java
+++ src/main/java/com/meerkat/ss/controller/ProductController.java
@@ -2,11 +2,13 @@ package com.meerkat.ss.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.logging.Logger;
 
 @Controller
+@RequestMapping("/product")
 public class ProductController {
 
     private Logger logger = Logger.getLogger(ProductController.class.getName());

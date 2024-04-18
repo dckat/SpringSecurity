b79afe1c8ee23a4212b037040077760a1e4912f7
dckat <kdc9619@naver.com>
2024-04-18 16:39:01 +0900

    chore: 불필요한 import 및 코드 제거

--- src/main/java/com/meerkat/ss/config/ProjectConfig.java
+++ src/main/java/com/meerkat/ss/config/ProjectConfig.java
@@ -1,27 +1,14 @@
 package com.meerkat.ss.config;
 
-import com.meerkat.ss.filter.AuthenticationLoggingFilter;
-import com.meerkat.ss.filter.CsrfTokenLogger;
-import com.meerkat.ss.filter.RequestValidationFilter;
-import com.meerkat.ss.filter.StaticKeyAuthenticationFilter;
-import com.meerkat.ss.service.AuthenticationProviderService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CsrfFilter;
 
 
 @Configuration
@@ -60,26 +47,6 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
                 .authorizeRequests()
                 .anyRequest().permitAll();
-    }
-    */
-
-    /*
-    @Autowired
-    private AuthenticationProviderService authenticationProvider;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authenticationProvider);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-
-        http.formLogin()
-                .defaultSuccessUrl("/main", true);
-
-        http.authorizeRequests()
-                .anyRequest().authenticated();
     }
     */
 }

1805eaa6c11eb016ac27917ecce3bdbe700e8e0f
dckat <kdc9619@naver.com>
2024-04-19 15:18:52 +0900

    feat: 토큰을 관리하기 위한 리포지토리 클래스 구현

---
+++ src/main/java/com/meerkat/ss/repository/JpaTokenRepository.java
@@ -0,0 +1,11 @@
+package com.meerkat.ss.repository;
+
+import com.meerkat.ss.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaTokenRepository extends JpaRepository<Token, Integer> {
+
+    Optional<Token> findTokenByIdentifier(String identifier);
+}

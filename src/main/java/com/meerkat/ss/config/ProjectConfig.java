1c9fc573e986310c56b87b539bc12d76c07810d9
dckat <kdc9619@naver.com>
2024-04-22 15:44:07 +0900

    chore: 불필요한 import. 클래스 제거

--- src/main/java/com/meerkat/ss/config/ProjectConfig.java
+++ src/main/java/com/meerkat/ss/config/ProjectConfig.java
@@ -1,16 +1,8 @@
 package com.meerkat.ss.config;
 
-import com.meerkat.ss.repository.CustomCsrfTokenRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
 
 
 @Configuration
@@ -23,63 +15,4 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .anyRequest().permitAll();
     }
-
-    /* JPA 활용 CSRF 토큰 구현 예제
-    @Bean
-    public CsrfTokenRepository csrfTokenRepository() {
-        return new CustomCsrfTokenRepository();
-    }
-
-    // CSRF 맞춤형 보호 사용
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf(c -> {
-            c.csrfTokenRepository(csrfTokenRepository());
-            c.ignoringAntMatchers("/ciao");
-        });
-
-        http.authorizeRequests()
-                .anyRequest().permitAll();
-    }
-    */
-
-    /* CSRF 보호 사용 예제 코드
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var uds = new InMemoryUserDetailsManager();
-
-        var u1 = User.withUsername("kim")
-                .password("12345")
-                .authorities("READ")
-                .build();
-
-        uds.createUser(u1);
-
-        return uds;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated();
-
-        http.formLogin()
-                .defaultSuccessUrl("/main", true);
-    }
-    */
-
-    /* CSRF 토큰 로깅을 위한 메소드
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-
-        http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
-                .authorizeRequests()
-                .anyRequest().permitAll();
-    }
-    */
 }

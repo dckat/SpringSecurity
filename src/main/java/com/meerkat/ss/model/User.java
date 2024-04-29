6b79fa76bfc857f35380889300b2d63a3dffa4ff
dckat <kdc9619@naver.com>
2024-04-29 16:40:36 +0900

    feat: 사용자 모델 클래스 구현

---
+++ src/main/java/com/meerkat/ss/model/User.java
@@ -0,0 +1,32 @@
+package com.meerkat.ss.model;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String code;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}

6ba3851f8903940f38aea5c79c418b6873f06769
dckat <kdc9619@naver.com>
2024-05-23 14:15:59 +0900

    feat: OAuth2 구현을 위한 구성클래스 구현

---
+++ src/main/java/com/meerkat/ss/config/ProjectConfig.java
@@ -0,0 +1,38 @@
+package com.meerkat.ss.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        var c = clientRegistration();
+        return new InMemoryClientRegistrationRepository(c);
+    }
+
+    // ClientRegistration 구현
+    private ClientRegistration clientRegistration() {
+        return CommonOAuth2Provider.GITHUB
+                .getBuilder("github")
+                .clientId("Ov23liqflbFdUkfct9md")
+                .clientSecret("d5c605327b7e692192facab6d7d32c2a0e2e57be")
+                .build();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.oauth2Login(c -> {
+            c.clientRegistrationRepository(clientRegistrationRepository());
+        });     // oauth2 인증 메소드 설정
+
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+}
